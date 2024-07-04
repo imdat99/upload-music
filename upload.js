28a5696dc8486b39c0964dd86c9941007171ab02
lethdat <imdat2999@gmail.com>
2024-07-05 04:56:05 +0700

    init

---
+++ upload.js
@@ -0,0 +1,55 @@
+import FormData from "form-data";
+import fs from "fs";
+import axios from "axios";
+
+const getkeyUrl = "https://helvid.net/upload/getkey";
+const uploadUrl = "https://vip.helvid.net/upload.php";
+const zmUrl = "https://helvid.com/index.php/ting";
+const cookie = JSON.parse(fs.readFileSync("cookie.json", "utf8")).cookies.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
+const client = axios.create({
+  headers: {
+    Cookie: cookie,
+  },
+});
+const getKey = () => {
+  const formData = new FormData();
+  const dataObj = { cid: "19", mycid: "0", fid: "18" };
+  for (const key in dataObj) {
+    formData.append(key, dataObj[key]);
+  }
+  return client.post(getkeyUrl, formData).then((res) => res.data);
+};
+
+const upload = (key, file) => {
+  const formData = new FormData();
+  const uploadObj = {
+    id: "WU_FILE_0",
+    type: "video/mp4",
+    video: file,
+  };
+  Object.keys(uploadObj).forEach((key) => {
+    formData.append(key, uploadObj[key]);
+  });
+
+  return client
+    .post(uploadUrl, formData, {
+      params: { key },
+      headers: formData.getHeaders(),
+    })
+        .then((res) => res.data);
+};
+
+export const uploadModule = async (url) => {
+  const {data} = await axios({
+    url: url,
+    method: "GET",
+    responseType: "stream",
+  });
+  const key = await getKey().then((r) => r.data);
+  console.log("uploadModule", url);
+  return await upload(key, data).then((r) => {
+      client.get(zmUrl, { params: { id: r.did } });
+      return { ...r };
+    })
+};
+
